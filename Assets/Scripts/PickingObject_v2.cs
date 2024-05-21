b3965ed45d6570b5fa4804439ea7fa34197859a8
nereaferrara02 <nerea.ferrara01@estudiant.upf.edu>
2024-05-21 22:10:14 +0200

    PickingObject_v2
    Another Version of the PickingObject (using rotation etc)

--- Assets/Scripts/PickingObject_v2.cs
+++ Assets/Scripts/PickingObject_v2.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickingObject : MonoBehaviour
+public class PickingObject_v2: MonoBehaviour
 {
     public List<GameObject> ingredientPrefabs;  //List of possible ingredients
     public float y;
     public GameObject ingredientInstance;
 
     public RotationPlayer playerRotation; // Reference to script de RotationPlayer
-    
+
     //Auxiliar Variables for the movement of the player
     public float rotationSpeed = 30f;
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
 
     //Variables for pattern Matching
     public float posMargin = 4f;
@@ -161,6 +161,74 @@ public class PickingObject : MonoBehaviour
     }
 
 
+    //------- Drop Ingredient ----------
+    private void DropIngredient()
+    {
+        // Remove the ingredient from the player
+        ingredientInstance.transform.parent = null;
+        StartCoroutine(DelayedFall(ingredientInstance));
+        ingredientInstance = null; // Set the ingredientInstance to null after dropping it in this way we can take another ingredient
+        // Reset the player's rotation to the initial rotation
+        playerRotation.ResetRotation();
+    }
+
+
+    //------- Corutine to make the ingredient fall ------------
+    IEnumerator DelayedFall(GameObject ingredient)
+    {
+        // Wait for 1 seconds
+        yield return new WaitForSeconds(1);
+
+        // Adding force to make the ingrendient fall
+        Rigidbody rb = ingredient.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(Vector3.down * 10, ForceMode.Impulse);
+        }
+
+        // Disable the isTrigger temporarily to allow for physics interaction
+        Collider collider = ingredient.GetComponent<Collider>();
+
+        if (collider != null) collider.isTrigger = false;
+
+        // Wait for 1 second before enabling trigger again
+        yield return new WaitForSeconds(1);
+
+        // Re-enable the Collider to allow picking it up again and isKinematic in this way the ingredient doesn't fall
+        if (collider != null) collider.isTrigger = true;
+
+        if (rb != null) rb.isKinematic = true;
+
+    }
+
+
+    //----- Function to pick up an object again -----
+    private void PickingAgainObject(Collider collision)
+    {
+        if (ingredientInstance == null)
+        {
+
+            if (collision.gameObject.CompareTag("Ingredient"))
+            {
+                int index = GetIngredientIndexByName(collision.gameObject.name);
+
+                print(index);
+
+                // Make the colliding object a child of the player
+                ingredientInstance = collision.gameObject;
+                ingredientInstance.transform.SetParent(transform);
+
+                // Reset position and rotation of the ingredient to match the player
+                Quaternion spawnRotation = (index == 0 || index == 2) ? Quaternion.Euler(90, 0, 0) : Quaternion.identity;
+                ingredientInstance.transform.localRotation = spawnRotation;
+                ingredientInstance.transform.localPosition = new Vector3(0, hand.transform.position.y - y, 0);
+
+
+            }
+        }
+    }
+
     //---- Searches for an ingredient in a list based on its name and returns its index ---
     public int GetIngredientIndexByName(string name)
     {
@@ -177,4 +245,4 @@ public class PickingObject : MonoBehaviour
 
 
 
-}
+}__
